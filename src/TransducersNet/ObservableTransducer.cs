932a932b49439deb3be5657bee5a060d0a2a8937
Bryan Murphy <bryan.murphy@gmail.com>
2014-09-30 22:06:37 -0500

    Move ObservableTransducer to a separate file

---
+++ src/TransducersNet/ObservableTransducer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reactive;
+
+namespace TransducersNet
+{
+    public class ObservableTransducer<TOut,TIn> : IObservable<TOut>
+    {
+        private ITransducer<TOut,TIn> transducer;
+        private IObservable<TIn> input;
+
+        public ObservableTransducer(ITransducer<TOut,TIn> transducer, IObservable<TIn> input)
+        {
+            this.transducer = transducer;
+            this.input = input;
+        }
+
+        public IDisposable Subscribe(IObserver<TOut> observer)
+        {
+            var collector = new IdentityReducer<TOut>();
+            var reducer = transducer.Transduce(collector);
+
+            var result = collector.Init();
+            var previous = result;
+
+            this.input.Subscribe(x => {
+                result = reducer.Apply(result, x);
+
+                if (result != previous)
+                    observer.OnNext(result.Value);
+
+                previous = result;
+            });
+
+            observer.OnCompleted();
+
+            return null;
+        }
+    }
+}
--- src/TransducersNet/Transducer.cs
+++ src/TransducersNet/Transducer.cs
@@ -1,43 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reactive;
 
 namespace TransducersNet
 {
-    public class ObservableTransducer<TOut,TIn> : IObservable<TOut>
-    {
-        private ITransducer<TOut,TIn> transducer;
-        private IObservable<TIn> input;
-
-        public ObservableTransducer(ITransducer<TOut,TIn> transducer, IObservable<TIn> input)
-        {
-            this.transducer = transducer;
-            this.input = input;
-        }
-
-        public IDisposable Subscribe(IObserver<TOut> observer)
-        {
-            var collector = new IdentityReducer<TOut>();
-            var reducer = transducer.Transduce(collector);
-
-            var result = collector.Init();
-            var previous = result;
-
-            this.input.Subscribe(x => {
-                result = reducer.Apply(result, x);
-
-                if (result != previous)
-                    observer.OnNext(result.Value);
-
-                previous = result;
-            });
-
-            observer.OnCompleted();
-
-            return null;
-        }
-    }
-
     public static class Transducer {
         public static IEnumerable<TOut> Enumerate<TIn,TOut>(ITransducer<TOut,TIn> transducer, IEnumerable<TIn> input) {
             var collector = new IdentityReducer<TOut>();
